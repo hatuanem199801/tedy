c6ddfcdb47d2e3a43e63c31ea63bacd0c85c7eb0
Tuan <32740978+hatuanem199801@users.noreply.github.com>
2021-05-03 11:18:42 +0700

    - Update layout admin.

--- components/LayoutAdmin.jsx
+++ components/LayoutAdmin.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Toaster } from "react-hot-toast";
 import { ChakraProvider, Container } from "@chakra-ui/react";
+import NavbarAdmin from "./NavbarAdmin";
 
 class LayoutAdmin extends Component {
   render() {
     return (
       <ChakraProvider>
         <Container maxW="container.xl" p={"10"}>
+          <NavbarAdmin />
           {this.props.children}
           <Toaster />
         </Container>
---
+++ components/NavbarAdmin.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
+import Logo from "./Logo";
+
+const Links = [
+  <a href="/admin/product">Sản phẩm</a>,
+  <a href="/admin/category">Loại sản phẩm</a>,
+  <a href="/admin/order">Hoá đơn</a>,
+  <a href="/admin/event">Sự kiện</a>,
+];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function NavbarAdmin() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
+  if (router.name !== "/" && router.pathname.indexOf("admin/login") === 1) {
+    return "";
+  }
+  return (
+    <>
+      <Box bg={"green.100"} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: !isOpen ? "none" : "inherit" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <a href="/admin/product">
+                <Logo name="TEDY" />
+              </a>
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+              >
+                <Avatar
+                  size={"sm"}
+                  src={
+                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>
+                  <a href="/api/logout">{"Đăng xuất"}</a>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
